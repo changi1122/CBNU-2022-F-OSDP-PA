de16c7af70d42ff08717ce03255f8d7649840e16
Woochang Lee <changi112242@gmail.com>
2022-09-23 21:13:02 +0900

    3장 실습문제 4번 출력 문제 수정

--- PROJECT220921/src/P4.java
+++ PROJECT220921/src/P4.java
@@ -8,6 +8,7 @@ public class P4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("소문자 알파벳 하나를 입력하세요>>");
         char alpha = sc.next().charAt(0);
 
         for (; 'a' <= alpha; alpha--) {
